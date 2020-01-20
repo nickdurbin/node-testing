22c3ed531c87974dee79bc8ad23d88da3cb727dc
ndurbin <nick.durbin@gmail.com>
2020-01-20 06:22:46 -0500

    Created an index for routes.

---
+++ api/routes/index.js
@@ -0,0 +1,7 @@
+const authRouter = require('./auth/auth-routes')
+const userRouter = require('./users/user-routes')
+
+module.exports = server => {
+  server.use("/api/auth", authRouter)
+  server.use("/api/users", userRouter)
+}

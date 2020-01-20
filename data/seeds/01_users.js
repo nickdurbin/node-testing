1183fb00bae40055ecf3dbba98e470e8b0068c45
ndurbin <nick.durbin@gmail.com>
2020-01-20 08:25:10 -0500

    Fixed a type for the seed data.

--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -1,8 +1,7 @@
-
 exports.seed = function(knex) {
   return knex('users')
     .then(function () {
-      return knex('table_name').insert([
+      return knex('users').insert([
         {id: 1, username: 'ndurbin', password: "password"},
         {id: 2, username: 'bobbieG', password: "password"},
         {id: 3, username: 'funnyGuy', password: "password"}

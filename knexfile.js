a029b216e25a0d40c26a13c1e754c2cc46d77b56
ndurbin <nick.durbin@gmail.com>
2020-01-20 06:21:36 -0500

    Added a testing object in knex

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,28 @@
 module.exports = {
-
   development: {
     client: 'sqlite3',
-    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/users.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   },
-
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };

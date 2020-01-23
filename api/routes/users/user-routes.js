2eaf8d76696f9b2a04346f9493a9ae5dfde89d56
ndurbin <nick.durbin@gmail.com>
2020-01-23 13:51:23 -0500

    Going to add full routing for users

--- api/routes/users/user-routes.js
+++ api/routes/users/user-routes.js
@@ -12,4 +12,36 @@ router.get("/", async (req, res, next) => {
   }
 })
 
+router.get("/:id", async (req, res, next) => {
+  try {
+   
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post("/", async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put("/:id", async (req, res, next) => {
+  try {
+   
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = router;

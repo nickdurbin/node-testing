b5bb17e32872e87377df471abfa15247151f4610
ndurbin <nick.durbin@gmail.com>
2020-01-23 12:31:41 -0500

    Big list of model tests.

--- api/routes/users/user-model.spec.js
+++ api/routes/users/user-model.spec.js
@@ -1,6 +1,11 @@
 const Users = require('./user-model')
 const db = require('../../../data/db-config')
 
+// re-run the seeds prior to every single test
+beforeEach(async () => {
+  await db.seed.run()
+})
+
 describe("users-model", () => {
   beforeEach(async () => {
     await db("users").truncate()
@@ -13,7 +18,29 @@ describe("users-model", () => {
       await Users.add({ username: "userJeff", password: "password"})
 
       const users = await db("users")
-      expect(users).toHaveLength(2)
+      expect(users).toHaveLength(5)
+    })
+
+    test("find", async () => {
+      const res = await Users.find()
+      expect(res.length).toBeGreaterThan(0)
+    })
+
+    test("findById", async () => {
+      const res = await Users.findById(1)
+      expect(res.username).toBe("userNick")
+    })
+
+    test("update", async () => {
+      await Users.update(1, { username: "userDurb" })
+      const user = await Users.findById(1)
+      expect(user.username).toBe("userDurb")
+    })
+
+    test("remove", async () => {
+      await Users.remove(1)
+      const users = await Users.find()
+      expect(users).toHaveLength(0)
     })
   })
 })
